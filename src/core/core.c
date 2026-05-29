cb03f291f1dd115a814ce0cc93b8b02bd8dd5f0e
veilriven-design <veilriven@gmail.com>
2026-05-29 17:25:39 -0500

    terse core.c

--- src/core/core.c
+++ src/core/core.c
@@ -1,22 +1,17 @@
 /*
  * core.c
  *
- * the actual l2 core protection domain. this is where authority comes from.
- * for now it's mostly a stub that will grow into the real thing.
+ * l2 core pd - authority root
  */
 
 #include <microkit.h>
 #include "../common/safe.h"
 
-void
-init(void)
-{
+void init(void) {
     microkit_dbg_puts("l2 core: alive\n");
 }
 
-void
-notified(microkit_channel ch)
-{
+void notified(microkit_channel ch) {
     (void)ch;
-    microkit_dbg_puts("l2 core: got a notification\n");
+    microkit_dbg_puts("l2 core: notified\n");
 }
